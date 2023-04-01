bcd08b9430ad4dda4df8245d13e30fd31ed40819
Diana <arazklycheva94@mail.ru>
2023-04-01 15:19:48 +0500

    Фото на память

--- js/preview.js
+++
@@ -1,7 +0,0 @@
-const imageChooser = document.querySelector('#upload-file');
-const imagePreview = document.querySelector('.img-upload__preview img');
-
-imageChooser.addEventListener('change', () => {
-  const image = imageChooser.files[0];
-  imagePreview.src = URL.createObjectURL(image);
-});
